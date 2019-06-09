044fff8e9167a8b03d3ae7cf71210fb13da0bf3a
nooice <imagetskanky@MacBook-Pro-2.local>
2019-06-08 22:32:58 -0600

    first commit

---
+++ client/build/precache-manifest.0180c2fe53f04e5e6a130160eabfc792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb21df19d8446e17c604",
+    "url": "/static/css/main.738fca18.chunk.css"
+  },
+  {
+    "revision": "cb21df19d8446e17c604",
+    "url": "/static/js/main.cb21df19.chunk.js"
+  },
+  {
+    "revision": "43a33ea9b5862b0054c4",
+    "url": "/static/js/1.43a33ea9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75205415bf10b9ba06127d3f4badcb5c",
+    "url": "/index.html"
+  }
+];
